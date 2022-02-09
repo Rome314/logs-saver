3c197920569aa00bf129d6e4a2ef1f9d2d4e94e1
Rome314 <401206176@mail.ru>
2022-02-09 12:35:59 +0400

    redis pub sub max len

--- pkg/connections/pubSub.go
+++ pkg/connections/pubSub.go
@@ -38,7 +38,7 @@ func GetRedisPubSub(ctx context.Context, client redis.UniversalClient, cfg Redis
 		ctx,
 		redisPubSub.PublisherConfig{
 			Maxlens: map[string]int64{
-				"events": 5000,
+				"events": 20000,
 			},
 		},
 		client,
